1b68b47e9b9eda97462af4be464419d910ecde2d
光天化日之下 <570929310@qq.com>
2025-06-16 14:21:00 +0800

    feat: change color

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <h1 style="color: blue">This is an home page</h1>
     <h2>hellow jenkis!</h2>
-    <h2>hellow docker!</h2>
+    <h2 style="color: red">hellow docker!</h2>
   </div>
 </template>
 
